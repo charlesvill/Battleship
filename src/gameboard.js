7e61340b0a6d3180d777c0e732a73fb674b3aca3
Charles Villalpando <charlesvill@g.ucla.edu>
2023-12-05 17:15:21 -0800

    Ship is able to be placed at coordinate

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,10 +1,37 @@
 const gameBoard = () => {
   let grid = [];
+  let ships = [];
   // initializer for the grid
   for(let i = 0; i < 10; i++){
+    grid[i] = [];
     for(let j = 0; j < 10; j++){
-      grid.push(null);
+      grid[i][j] = null;
     }
   }
 
+  function shipFits(length, coordinates, orientation){
+
+    const r = coordinates[0];
+    const c = coordinates[1];
+    let space = 0;
+
+    // h horizontal : v vertical
+    if(orientation = 'h'){
+      space = grid[0].length;
+    } else if(orientation = 'v'){
+
+    }
+  }
+
+  function addShip(ship, coordinates){
+    const length = ship.length;
+    const row = coordinates[0];
+    const column = coordinates[1];
+    let orientation = 'v';
+    // check if a move is legal per the coordinate
+    grid[row][column] = ship;
+  }
+  return {grid, ships, addShip};
 }
+const test = gameBoard();
+module.exports = gameBoard;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -7,6 +7,15 @@ const frigateTest = ship(4);
 const testBoard = gameboard();
 
 test("ship placed at coordinate", () => {
-  frigateTest.addShip(frigateTest, 1, 2);
-  expect(frigateTest.checkGrid(1, 2)).not.toBe(null);
+  testBoard.addShip(frigateTest, [1, 2]);
+  expect(testBoard.grid[1][2]).not.toBe(null);
 });
+
+
+test("ship correctly laid out", () => {
+
+});
+
+test("recieve Attack hits a ship", () => {
+
+})
