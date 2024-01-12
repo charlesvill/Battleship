cd828af9c6e6c5c70819a0ecd41d420fa9173e80
Charles Villalpando <charlesvill@g.ucla.edu>
2024-01-11 20:46:02 -0800

    Made shipscreen function async to wait for user inputs

--- src/ui.js
+++ src/ui.js
@@ -92,9 +92,11 @@ const userInterface = (shipMakerProxy, playerInitScript, gameInitScript) => {
     });
   }
 
-  function shipScreen(playerObj) {
-    // clear page container and populate with ship select
-    const htmlContent = `
+  async function shipScreen(playerObj) {
+    // need async function to wait for each player ship selection to be resolved before moving on to the next one
+    return new Promise((resolve) => {
+      // clear page container and populate with ship select
+      const htmlContent = `
       <div class="shipScreenCont">
           <div class="headerCont">
               <div class="playerName">
@@ -138,252 +140,256 @@ const userInterface = (shipMakerProxy, playerInitScript, gameInitScript) => {
           </div>
       </div>
      `;
-    pageContainer.innerHTML = "";
-    pageContainer.innerHTML = htmlContent;
+      pageContainer.innerHTML = "";
+      pageContainer.innerHTML = htmlContent;
 
-    // necessary globals for methods in ship select
-    const gridContainer = document.querySelector(".gridCont");
-    const gridSize = 10;
-    let dragShipLength = 0;
-    let dragShip = undefined;
-    let dragFits = false;
-    let orientation = "h";
-    let coord = [];
-    let mowCount = 1;
-    let frigCount = 2;
-    let schoonCount = 3;
-    let sloopCount = 2;
-    let depletedShip = null;
+      // necessary globals for methods in ship select
+      const gridContainer = document.querySelector(".gridCont");
+      const gridSize = 10;
+      let dragShipLength = 0;
+      let dragShip = undefined;
+      let dragFits = false;
+      let orientation = "h";
+      let coord = [];
+      let mowCount = 1;
+      let frigCount = 2;
+      let schoonCount = 3;
+      let sloopCount = 2;
+      let depletedShip = null;
+      console.log(`the current player is: ${playerObj.number}`);
 
-    let ships = document.querySelectorAll(".ship");
-    let shipContainer = document.querySelector(".shipBox");
-    let manCountBox = document.querySelector(".shipCount.man");
-    let frigCountBox = document.querySelector(".shipCount.frig");
-    let schoonCountBox = document.querySelector(".shipCount.schoon");
-    let sloopCountBox = document.querySelector(".shipCount.sloop");
+      let ships = document.querySelectorAll(".ship");
+      let shipContainer = document.querySelector(".shipBox");
+      let manCountBox = document.querySelector(".shipCount.man");
+      let frigCountBox = document.querySelector(".shipCount.frig");
+      let schoonCountBox = document.querySelector(".shipCount.schoon");
+      let sloopCountBox = document.querySelector(".shipCount.sloop");
 
-    manCountBox.textContent = `x ${mowCount}`;
-    frigCountBox.textContent = `x ${frigCount}`;
-    schoonCountBox.textContent = `x ${schoonCount}`;
-    sloopCountBox.textContent = `x ${sloopCount}`;
-    // build the visual grid
-    for (let i = 0; i < gridSize; i++) {
-      const row = document.createElement("div");
-      row.classList.add("rowCont");
-      gridContainer.appendChild(row);
+      manCountBox.textContent = `x ${mowCount}`;
+      frigCountBox.textContent = `x ${frigCount}`;
+      schoonCountBox.textContent = `x ${schoonCount}`;
+      sloopCountBox.textContent = `x ${sloopCount}`;
+      // build the visual grid
+      for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("rowCont");
+        gridContainer.appendChild(row);
 
-      for (let j = 0; j < gridSize; j++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.dataset.r = i;
-        cell.dataset.c = j;
-        row.appendChild(cell);
-      }
-    }
-    // cycle ship placement orientation, initialized to "h"
-    const orientationBtn = document.querySelector(".orientationBtn");
-    orientationBtn.addEventListener("click", (e) => {
-      if (orientation === "h") {
-        e.currentTarget.dataset.orientation = "v";
-        orientation = "v";
-        orientationBtn.textContent = "Vertical";
-      } else {
-        e.currentTarget.dataset.orientation = "h";
-        orientation = "h";
-        orientationBtn.textContent = "Horizontal";
-      }
-    });
-
-    const gridShader = (
-      coord,
-      length,
-      orientation,
-      dragFits,
-      placed = false,
-    ) => {
-      const offsetr = orientation === "h" ? 0 : 1;
-      const offsetc = orientation === "h" ? 1 : 0;
-      let addedClass = "";
-
-      // 3 shading possiblities fits/nofits/placed
-      if (placed === true) {
-        addedClass = "placed";
-      } else {
-        addedClass = dragFits === true ? "fits" : "notFits";
-      }
-
-      const currentCoord = [...coord];
-      let cellCollection = [];
-
-      // shade each cell representing ship length
-      for (let i = 0; i < length; i++) {
-        const currentCell = document.querySelector(
-          `[data-r="${currentCoord[0]}"][data-c="${currentCoord[1]}"]`,
-        );
-        cellCollection.push(currentCell);
-
-        if (currentCell !== null) {
-          currentCell.classList.add(`${addedClass}`);
-        } else {
-          continue;
+        for (let j = 0; j < gridSize; j++) {
+          const cell = document.createElement("div");
+          cell.classList.add("cell");
+          cell.dataset.r = i;
+          cell.dataset.c = j;
+          row.appendChild(cell);
         }
-        currentCoord[0] += offsetr;
-        currentCoord[1] += offsetc;
       }
-      // after shade, dragleave handler to clear shading when not placed
-      const firstCell = cellCollection[0];
-      if (firstCell === null || firstCell === undefined || placed === true) {
-        return;
-      }
-      firstCell.addEventListener("dragleave", (e) => {
-        e.preventDefault();
-        cellCollection.forEach((element) => {
-          if (element !== null) {
-            element.classList.remove(`${addedClass}`);
-          }
-        });
+      // cycle ship placement orientation, initialized to "h"
+      const orientationBtn = document.querySelector(".orientationBtn");
+      orientationBtn.addEventListener("click", (e) => {
+        if (orientation === "h") {
+          e.currentTarget.dataset.orientation = "v";
+          orientation = "v";
+          orientationBtn.textContent = "Vertical";
+        } else {
+          e.currentTarget.dataset.orientation = "h";
+          orientation = "h";
+          orientationBtn.textContent = "Horizontal";
+        }
       });
-    };
 
-    function leaveScreen() {
-      return;
-    }
+      const gridShader = (
+        coord,
+        length,
+        orientation,
+        dragFits,
+        placed = false,
+      ) => {
+        const offsetr = orientation === "h" ? 0 : 1;
+        const offsetc = orientation === "h" ? 1 : 0;
+        let addedClass = "";
 
-    const cells = document.querySelectorAll(".cell");
-    // translates UI cell to a coordinate on a dragover event
-    // checks if the ship dragged will fit
-    cells.forEach((cell) => {
-      const dragOverHandler = (e) => {
-        if (dragShipLength === undefined) {
+        // 3 shading possiblities fits/nofits/placed
+        if (placed === true) {
+          addedClass = "placed";
+        } else {
+          addedClass = dragFits === true ? "fits" : "notFits";
+        }
+
+        const currentCoord = [...coord];
+        let cellCollection = [];
+
+        // shade each cell representing ship length
+        for (let i = 0; i < length; i++) {
+          const currentCell = document.querySelector(
+            `[data-r="${currentCoord[0]}"][data-c="${currentCoord[1]}"]`,
+          );
+          cellCollection.push(currentCell);
+
+          if (currentCell !== null) {
+            currentCell.classList.add(`${addedClass}`);
+          } else {
+            continue;
+          }
+          currentCoord[0] += offsetr;
+          currentCoord[1] += offsetc;
+        }
+        // after shade, dragleave handler to clear shading when not placed
+        const firstCell = cellCollection[0];
+        if (firstCell === null || firstCell === undefined || placed === true) {
           return;
         }
-        e.preventDefault();
-
-        cell.classList.add("mouseover");
-
-        const r = Number(e.currentTarget.dataset.r);
-        const c = Number(e.currentTarget.dataset.c);
-        coord = [r, c];
-        console.log(`coord before proxy: ${coord}`);
-        dragFits = shipMakerProxy(
-          playerObj.number,
-          dragShipLength,
-          coord,
-          orientation,
-          true,
-        );
-        console.log(`coord post shipmaker: ${coord}`);
-        if (dragFits) {
-          // add classname for fits
-          gridShader(coord, dragShipLength, orientation, dragFits, false);
-        } else {
-          // add classname for not fits
-          gridShader(coord, dragShipLength, orientation, dragFits, false);
-        }
-        coordCalculated = true;
-        cell.removeEventListener("dragover", dragOverHandler);
+        firstCell.addEventListener("dragleave", (e) => {
+          e.preventDefault();
+          cellCollection.forEach((element) => {
+            if (element !== null) {
+              element.classList.remove(`${addedClass}`);
+            }
+          });
+        });
       };
 
-      cell.addEventListener("dragover", dragOverHandler);
-      cell.addEventListener("dragleave", (e) => {
-        coordCalculated = false;
-        cell.classList.remove("mouseover");
-        cell.addEventListener("dragover", dragOverHandler);
-      });
-    });
+      function leaveScreen() {
+        return;
+      }
 
-    const shipIMG = new Image();
-    shipIMG.src = "./images/sailboat.png";
-    shipIMG.classList.add("shipIMG");
-    shipIMG.style.width = "1rem";
+      const cells = document.querySelectorAll(".cell");
+      // translates UI cell to a coordinate on a dragover event
+      // checks if the ship dragged will fit
+      cells.forEach((cell) => {
+        const dragOverHandler = (e) => {
+          if (dragShipLength === undefined) {
+            return;
+          }
+          e.preventDefault();
 
-    ships.forEach((ship) => {
-      const shipDragHandler = (e) => {
-        dragShipLength = Number(e.currentTarget.dataset.index);
+          cell.classList.add("mouseover");
 
-        const clone = ship.cloneNode(true);
-        dragShip = ship;
-        // Set the offset for the drag image
-        const offsetX = 20; // Set your desired offset value
-        e.dataTransfer.setDragImage(clone, 0, 0);
-        ship.classList.add("dragging");
-      };
-
-      ship.addEventListener("dragstart", shipDragHandler);
-
-      ship.addEventListener("dragend", () => {
-        ship.classList.remove("dragging");
-
-        if (dragFits) {
-          console.log(`coord before placing is : ${coord}`);
-          const placed = shipMakerProxy(
+          const r = Number(e.currentTarget.dataset.r);
+          const c = Number(e.currentTarget.dataset.c);
+          coord = [r, c];
+          console.log(`coord before proxy: ${coord}`);
+          dragFits = shipMakerProxy(
             playerObj.number,
             dragShipLength,
             coord,
             orientation,
-            false,
+            true,
           );
+          console.log(`coord post shipmaker: ${coord}`);
+          if (dragFits) {
+            // add classname for fits
+            gridShader(coord, dragShipLength, orientation, dragFits, false);
+          } else {
+            // add classname for not fits
+            gridShader(coord, dragShipLength, orientation, dragFits, false);
+          }
+          coordCalculated = true;
+          cell.removeEventListener("dragover", dragOverHandler);
+        };
 
-          console.log(`coord after placing is : ${coord}`);
-          if (placed) {
-            gridShader(coord, dragShipLength, orientation, dragFits, true);
+        cell.addEventListener("dragover", dragOverHandler);
+        cell.addEventListener("dragleave", (e) => {
+          coordCalculated = false;
+          cell.classList.remove("mouseover");
+          cell.addEventListener("dragover", dragOverHandler);
+        });
+      });
 
-            let remainingShips = "";
+      const shipIMG = new Image();
+      shipIMG.src = "./images/sailboat.png";
+      shipIMG.classList.add("shipIMG");
+      shipIMG.style.width = "1rem";
 
-            switch (dragShipLength) {
-              case 5:
-                remainingShips = mowCount;
-                mowCount -= 1;
-                manCountBox.textContent = `x ${mowCount}`;
-                break;
-              case 4:
-                remainingShips = frigCount;
-                frigCount -= 1;
-                frigCountBox.textContent = `x ${frigCount}`;
-                break;
-              case 3:
-                remainingShips = schoonCount;
-                schoonCount -= 1;
-                schoonCountBox.textContent = `x ${schoonCount}`;
-                break;
-              case 2:
-                remainingShips = sloopCount;
-                sloopCount -= 1;
-                sloopCountBox.textContent = `x ${sloopCount}`;
-                break;
-              default:
-                console.error("error: invalid ship length in dragShip");
-            }
-            remainingShips -= 1;
+      ships.forEach((ship) => {
+        const shipDragHandler = (e) => {
+          dragShipLength = Number(e.currentTarget.dataset.index);
 
-            if (remainingShips <= 0) {
-              ship.classList.add("depleted");
-              ship.removeEventListener("dragstart", shipDragHandler);
-              ship.draggable = false;
+          const clone = ship.cloneNode(true);
+          dragShip = ship;
+          // Set the offset for the drag image
+          const offsetX = 20; // Set your desired offset value
+          e.dataTransfer.setDragImage(clone, 0, 0);
+          ship.classList.add("dragging");
+        };
+
+        ship.addEventListener("dragstart", shipDragHandler);
+
+        ship.addEventListener("dragend", () => {
+          ship.classList.remove("dragging");
+
+          if (dragFits) {
+            console.log(`coord before placing is : ${coord}`);
+            const placed = shipMakerProxy(
+              playerObj.number,
+              dragShipLength,
+              coord,
+              orientation,
+              false,
+            );
+
+            console.log(`coord after placing is : ${coord}`);
+            if (placed) {
+              gridShader(coord, dragShipLength, orientation, dragFits, true);
+
+              let remainingShips = "";
+
+              switch (dragShipLength) {
+                case 5:
+                  remainingShips = mowCount;
+                  mowCount -= 1;
+                  manCountBox.textContent = `x ${mowCount}`;
+                  break;
+                case 4:
+                  remainingShips = frigCount;
+                  frigCount -= 1;
+                  frigCountBox.textContent = `x ${frigCount}`;
+                  break;
+                case 3:
+                  remainingShips = schoonCount;
+                  schoonCount -= 1;
+                  schoonCountBox.textContent = `x ${schoonCount}`;
+                  break;
+                case 2:
+                  remainingShips = sloopCount;
+                  sloopCount -= 1;
+                  sloopCountBox.textContent = `x ${sloopCount}`;
+                  break;
+                default:
+                  console.error("error: invalid ship length in dragShip");
+              }
+              remainingShips -= 1;
+
+              if (remainingShips <= 0) {
+                ship.classList.add("depleted");
+                ship.removeEventListener("dragstart", shipDragHandler);
+                ship.draggable = false;
+              }
             }
           }
-        }
-        dragShip = undefined;
-        dragShipLength = undefined;
-        if (
-          mowCount <= 0 &&
-          frigCount <= 0 &&
-          schoonCount <= 0 &&
-          sloopCount <= 0
-        ) {
-          const nextBtn = document.createElement("button");
-          nextBtn.textContent = "Next";
-          pageContainer.appendChild(nextBtn);
+          dragShip = undefined;
+          dragShipLength = undefined;
+          if (
+            mowCount <= 0 &&
+            frigCount <= 0 &&
+            schoonCount <= 0 &&
+            sloopCount <= 0
+          ) {
+            const nextBtn = document.createElement("button");
+            nextBtn.textContent = "Next";
+            pageContainer.appendChild(nextBtn);
 
-          nextBtn.addEventListener("click", () => {
-            leaveScreen();
-            console.log("there should be some leaving happening right now");
-          });
-        }
+            nextBtn.addEventListener("click", () => {
+              resolve();
+              console.log(
+                "there should be some resolving of promises happening right now",
+              );
+            });
+          }
+        });
       });
     });
   }
-  function startScreen(gameScriptFn) {
+  async function startScreen(gameScriptFn) {
     const htmlContent = `
       <div class="title">Battleship</div>
               <div class="playerSelectCont">
@@ -448,17 +454,19 @@ const userInterface = (shipMakerProxy, playerInitScript, gameInitScript) => {
         "selectp1",
         "selectp2",
       );
-
-      players.forEach((element) => {
-        if (element.player === "person") {
-          playerInitScript(element);
-          shipScreen(element);
-        } else {
-          playerInitScript(element);
-          shipRandomizer(element);
+      async function processPlayers(players) {
+        for (const element of players) {
+          if (element.player === "person") {
+            playerInitScript(element);
+            console.log("player screen being opened");
+            await shipScreen(element);
+          } else {
+            playerInitScript(element);
+            shipRandomizer(element);
+          }
         }
-      });
-      // trigger the next screen
+      }
+      processPlayers(players);
     });
   }
   return { startScreen, pObjInitializer };
